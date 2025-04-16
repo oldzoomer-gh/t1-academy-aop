9d560396071a120dfbe92330bb32e592beda5452
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-04-16 09:31:02 +0300

    Fixing the 3.0 release

--- t1-academy-aop-logger/src/main/java/ru/gavrilovegor519/t1_academy_aop_logger/aspects/LogAspects.java
+++ t1-academy-aop-logger/src/main/java/ru/gavrilovegor519/t1_academy_aop_logger/aspects/LogAspects.java
@@ -1,29 +1,35 @@
 package ru.gavrilovegor519.t1_academy_aop_logger.aspects;
 
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import ru.gavrilovegor519.t1_academy_aop_logger.props.LogLevel;
 
 @Aspect
 @Log4j2
-@Setter
+@RequiredArgsConstructor
 public class LogAspects {
-    private String level;
+    private final LogLevel logLevel;
 
-    @AfterThrowing(value = "@annotation(ru.gavrilovegor519.t1_academy_aop.annotations.LogExecution)", throwing = "throwable")
+    @Pointcut("@annotation(ru.gavrilovegor519.t1_academy_aop.annotations.LogExecution)")
+    public void logExecution() {
+    }
+
+    @AfterThrowing(pointcut = "logExecution()", throwing = "throwable")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable throwable) {
         log.error("Exception thrown in method :{} with message: {}", joinPoint.getSignature().getName(), throwable.getMessage());
     }
 
-    @Around("@annotation(ru.gavrilovegor519.t1_academy_aop.annotations.LogExecution)")
+    @Around("logExecution()")
     public Object logAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        if (level.equals("info")) log.info("Method called: {}", proceedingJoinPoint.getSignature().getName());
+        if (logLevel.isInfo()) log.info("Method called: {}", proceedingJoinPoint.getSignature().getName());
         Object result = proceedingJoinPoint.proceed();
-        if (level.equals("info"))
+        if (logLevel.isInfo())
             log.info("Method execution is finished: {}", proceedingJoinPoint.getSignature().getName());
         return result;
     }
