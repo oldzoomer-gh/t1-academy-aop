2513ee53f44f0c9402eb40ed68cff5298dc757dc
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-04-21 22:10:05 +0300

    Some additional fixes.

--- t1-academy-aop-logger/src/main/java/ru/gavrilovegor519/t1_academy_aop_logger/aspects/LogAspects.java
+++ t1-academy-aop-logger/src/main/java/ru/gavrilovegor519/t1_academy_aop_logger/aspects/LogAspects.java
@@ -1,36 +1,35 @@
 package ru.gavrilovegor519.t1_academy_aop_logger.aspects;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import ru.gavrilovegor519.t1_academy_aop_logger.props.LogLevel;
+import org.slf4j.event.Level;
+import ru.gavrilovegor519.t1_academy_aop_logger.props.LogProps;
 
 @Aspect
-@Log4j2
+@Slf4j
 @RequiredArgsConstructor
 public class LogAspects {
-    private final LogLevel logLevel;
+    private final LogProps logProps;
 
-    @Pointcut("@annotation(ru.gavrilovegor519.t1_academy_aop.annotations.LogExecution)")
-    public void logExecution() {
-    }
-
-    @AfterThrowing(pointcut = "logExecution()", throwing = "throwable")
+    @AfterThrowing(value = "@annotation(ru.gavrilovegor519.t1_academy_aop_logger.aspects.LogAspects)", throwing = "throwable")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable throwable) {
-        log.error("Exception thrown in method :{} with message: {}", joinPoint.getSignature().getName(), throwable.getMessage());
+        log.atLevel(Level.valueOf(logProps.getAtLevel()))
+                .log("Exception thrown in method :{} with message: {}",
+                        joinPoint.getSignature().getName(), throwable.getMessage());
     }
 
-    @Around("logExecution()")
+    @Around("@annotation(ru.gavrilovegor519.t1_academy_aop_logger.aspects.LogAspects)")
     public Object logAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        if (logLevel.isInfo()) log.info("Method called: {}", proceedingJoinPoint.getSignature().getName());
+        log.atLevel(Level.valueOf(logProps.getAtLevel()))
+                .log("Method called: {}", proceedingJoinPoint.getSignature().getName());
         Object result = proceedingJoinPoint.proceed();
-        if (logLevel.isInfo())
-            log.info("Method execution is finished: {}", proceedingJoinPoint.getSignature().getName());
+        log.atLevel(Level.valueOf(logProps.getAtLevel()))
+                .log("Method execution is finished: {}", proceedingJoinPoint.getSignature().getName());
         return result;
     }
 }
--- t1-academy-aop-logger/src/main/java/ru/gavrilovegor519/t1_academy_aop_logger/config/LogConfig.java
+++ t1-academy-aop-logger/src/main/java/ru/gavrilovegor519/t1_academy_aop_logger/config/LogConfig.java
@@ -1,19 +1,26 @@
 package ru.gavrilovegor519.t1_academy_aop_logger.config;
 
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.event.Level;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import ru.gavrilovegor519.t1_academy_aop_logger.aspects.LogAspects;
-import ru.gavrilovegor519.t1_academy_aop_logger.props.LogLevel;
+import ru.gavrilovegor519.t1_academy_aop_logger.props.LogProps;
 
 @Configuration
-@EnableConfigurationProperties(LogLevel.class)
-@Log4j2
+@EnableConfigurationProperties(LogProps.class)
+@EnableAspectJAutoProxy
+@Slf4j
 public class LogConfig {
     @Bean
-    public LogAspects logAspect(LogLevel logLevel) {
-        log.info("Log level: {}", logLevel.getLevel());
-        return new LogAspects(logLevel);
+    @ConditionalOnProperty(prefix = "log-aspects", name = "enable",
+            havingValue = "true", matchIfMissing = true)
+    public LogAspects logAspect(LogProps logProps) {
+        log.atLevel(Level.valueOf(logProps.getAtLevel()))
+                .log("LogAspects is enabled");
+        return new LogAspects(logProps);
     }
 }
